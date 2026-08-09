f75945e19311e7849b569304eba883bc81098573
Ferdi Durgun <ferdidurgun34@gmail.com>
2026-08-09 05:50:44 +0300

    Update EarthquakeApplication.kt

--- app/src/main/java/com/ferdidrgn/anlikdepremler/EarthquakeApplication.kt
+++ app/src/main/java/com/ferdidrgn/anlikdepremler/EarthquakeApplication.kt
@@ -1,7 +1,18 @@
 package com.ferdidrgn.anlikdepremler
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class EarthquakeApplication : Application()
+class EarthquakeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        try {
+            FirebaseApp.initializeApp(this)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
